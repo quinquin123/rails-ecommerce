bd8c1687c6eb747d02c7dbd9654564f7097cc185
Bui Quynh <trickingquinquin@gmail.com>
2025-08-11 10:56:54 +0700

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_11_030122) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_11_024920) do
   create_schema "quynh_schema"
 
   # These are extensions that must be enabled in order to support this database
